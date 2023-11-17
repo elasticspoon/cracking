7989b0534a054e52768318f513523ad9106125f0
YuriBocharov <quesadillaman@gmail.com>
2023-11-16 19:46:45 -0500

    5.4, 5.6, 5.7

--- chapter5/chapter5.go
+++ chapter5/chapter5.go
@@ -1,1 +1,86 @@
 package chapter5
+
+func nextLarger(n int) int {
+	length := 0
+	numBits := 0
+	neededBitPos := -1
+
+	for i := n; i > 0; i /= 2 {
+		if i&1 == 1 {
+			numBits++
+		}
+		if (i^0b10)&0b11 == 0b11 && neededBitPos == -1 {
+			neededBitPos = length
+		}
+		length++
+	}
+
+	if numBits == 0 {
+		return -1
+	}
+
+	if neededBitPos == length-1 {
+		return 1<<length + 1<<(numBits-1) - 1
+	}
+
+	return n - (1 << neededBitPos) + (1 << (neededBitPos + 1))
+}
+
+func nextSmaller(n int) int {
+	length := 0
+
+	for i := n; i > 0; i /= 2 {
+		length++
+		if (i^0b01)&0b11 == 0b11 {
+			return n - (1 << length) + (1 << (length - 1))
+		}
+	}
+
+	return -1
+}
+
+func bookNextLarger(n int) int {
+	var c0, c1 int
+	c := n
+
+	for (c&1) == 0 && (c != 0) {
+		c0++
+		c >>= 1
+	}
+
+	for (c & 1) == 1 {
+		c1++
+		c >>= 1
+	}
+
+	if c0+c1 == 31 || c0+c1 == 0 {
+		return -1
+	}
+
+	p := c0 + c1
+	n |= (1 << p)
+	n &= ^((1 << p) - 1)
+	n |= (1 << (c1 - 1)) - 1
+	return n
+}
+
+func BitsToFlip(a, b int) int {
+	count := 0
+	for c := a ^ b; c != 0; c >>= 1 {
+		count += c & 1
+	}
+	return count
+}
+
+func SwapOddEvenBits(n int) int {
+	evenMask := 0
+	for i := 0; i < 63; i += 2 {
+		evenMask |= 1 << i
+	}
+	oddMask := evenMask << 1
+
+	evens := n & evenMask
+	odds := n & oddMask
+
+	return (evens << 1) | (odds >> 1)
+}
--- chapter5/chapter5_test.go
+++ chapter5/chapter5_test.go
@@ -1,1 +1,112 @@
 package chapter5
+
+import "testing"
+
+func TestBookNextLarger(t *testing.T) {
+	tests := []struct {
+		given    int
+		expected int
+	}{
+		{0b11101, 0b11110},
+		{0b11011010101011, 0b11011010101101},
+		{0b100, 0b1000},
+		{0b1111000, 0b10000111},
+		{0b111, 0b1011},
+		{0b0, -1},
+	}
+
+	for _, tt := range tests {
+		actual := bookNextLarger(tt.given)
+		if actual != tt.expected {
+			t.Errorf("nextLarger(%b): expected %b, actual %b", tt.given, tt.expected, actual)
+		}
+	}
+}
+
+func TestNextLarger(t *testing.T) {
+	tests := []struct {
+		given    int
+		expected int
+	}{
+		{0b11101, 0b11110},
+		{0b11011010101011, 0b11011010101101},
+		{0b100, 0b1000},
+		{0b1111000, 0b10000111},
+		{0b111, 0b1011},
+		{0b0, -1},
+	}
+
+	for _, tt := range tests {
+		actual := nextLarger(tt.given)
+		if actual != tt.expected {
+			t.Errorf("nextLarger(%b): expected %b, actual %b", tt.given, tt.expected, actual)
+		}
+	}
+}
+
+func TestNextSmaller(t *testing.T) {
+	tests := []struct {
+		given    int
+		expected int
+	}{
+		{0b0, -1},
+		{0b1, -1},
+		{0b111, -1},
+		{0b1011, 0b111},
+		{0b1000, 0b100},
+		{0b10000111, 0b1000111},
+		{0b10110111, 0b10101111},
+	}
+
+	for _, tt := range tests {
+		actual := nextSmaller(tt.given)
+		if actual != tt.expected {
+			t.Errorf("nextLarger(%b): expected %b, actual %b", tt.given, tt.expected, actual)
+		}
+	}
+}
+
+func TestBitsToFlip(t *testing.T) {
+	tests := []struct {
+		a, b     int
+		expected int
+	}{
+		{0b11101, 0b11110, 2},
+		{0b11011010101011, 0b11011010101101, 2},
+		{0b100, 0b1000, 2},
+		{0b1111000, 0b10000111, 8},
+		{0b10000111, 0b1111000, 8},
+		{0b111, 0b1011, 2},
+		{0b1011, 0b111, 2},
+	}
+
+	for _, tt := range tests {
+		actual := BitsToFlip(tt.a, tt.b)
+		if actual != tt.expected {
+			t.Errorf("bitsToFlip(%b, %b): expected %d, actual %d", tt.a, tt.b, tt.expected, actual)
+		}
+	}
+}
+
+func TestSwapOddEvenBits(t *testing.T) {
+	tests := []struct {
+		given    int
+		expected int
+	}{
+		{0b10101010, 0b01010101},
+		{0b11100011, 0b11010011},
+		{0b0, 0b0},
+		{0b1, 0b10},
+		{0b11, 0b11},
+		{0b111, 0b1011},
+		{0b1111, 0b1111},
+		{-1, -1},
+	}
+
+	for _, tt := range tests {
+		actual := SwapOddEvenBits(tt.given)
+		if actual != tt.expected {
+			t.Errorf("swapOddEvenBits(%b): expected %b, actual %b", tt.given, tt.expected, actual)
+		}
+	}
+}
